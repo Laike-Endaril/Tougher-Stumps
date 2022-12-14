f1bcb62e267232ed1ff19cdfcb8bf8c46390461f
Kyle Koder <silvertallon@gmail.com>
2022-12-14 15:48:27 -0800

    1.12.2.000b; Fixed Z fighting by editing models a bit (at least with most common block shapes)

--- src/main/java/com/fantasticsource/tougherstumps/TougherStumps.java
+++ src/main/java/com/fantasticsource/tougherstumps/TougherStumps.java
@@ -25,7 +25,7 @@ public class TougherStumps
 {
     public static final String MODID = "tougherstumps";
     public static final String NAME = "Tougher Stumps";
-    public static final String VERSION = "1.12.2.000a";
+    public static final String VERSION = "1.12.2.000b";
 
     protected static ArrayList<ChunkPos> done = new ArrayList<>();
 

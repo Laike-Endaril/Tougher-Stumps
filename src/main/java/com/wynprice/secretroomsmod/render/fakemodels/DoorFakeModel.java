dbaf1fcbf3eb3b61578deeddd0b413a5e7cd4c1c
Kyle Koder <silvertallon@gmail.com>
2022-12-16 16:27:33 -0800

    More code cleanup

--- src/main/java/com/wynprice/secretroomsmod/render/fakemodels/DoorFakeModel.java
+++ src/main/java/com/wynprice/secretroomsmod/render/fakemodels/DoorFakeModel.java
@@ -21,72 +21,43 @@ public class DoorFakeModel extends FakeBlockModel
         super(model);
     }
 
-    public IBlockState getNormalStateWith(IBlockState s)
-    {
-        return Blocks.DARK_OAK_DOOR.getDefaultState().withProperty(BlockDoor.FACING, s.getValue(BlockDoor.FACING)).withProperty(BlockDoor.HALF, s.getValue(BlockDoor.HALF))
-                .withProperty(BlockDoor.HINGE, s.getValue(BlockDoor.HINGE)).withProperty(BlockDoor.OPEN, s.getValue(BlockDoor.OPEN)).withProperty(BlockDoor.POWERED, s.getValue(BlockDoor.POWERED));
-    }
-
-    protected Class<? extends BaseBlockDoor> getBaseBlockClass()
-    {
-        return BaseBlockDoor.class;
-    }
-
-    protected EnumFacing[] fallbackOrder()
-    {
-        EnumFacing[] list = new EnumFacing[EnumFacing.VALUES.length + 1];
-        for (int i = 0; i < EnumFacing.VALUES.length; i++)
-            list[i] = EnumFacing.VALUES[i];
-        list[EnumFacing.VALUES.length] = null;
-        return list;
-    }
-
-    protected RenderInfo getRenderInfo(EnumFacing face, IBlockState teMirrorState)
-    {
-        return new RenderInfo(teMirrorState, getModel(teMirrorState));
-    }
-
     @Override
     public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand)
     {
-        if (getBaseBlockClass() != null && !(getBaseBlockClass().isAssignableFrom(BaseTERender.currentRender.getBlock().getClass())))
-            return super.getQuads(state, side, rand);
+        if (!BaseBlockDoor.class.isAssignableFrom(BaseTERender.currentRender.getBlock().getClass())) return super.getQuads(state, side, rand);
+
+
         ArrayList<BakedQuad> finalList = new ArrayList<>();
-        RenderInfo renderInfo = getRenderInfo(side, ((TileEntityInfomationHolder) BaseTERender.currentWorld.getTileEntity(BaseTERender.currentPos)).getMirrorState());
-        IBlockState normalState = getNormalStateWith(BaseTERender.currentRender);
-        if (renderInfo != null)
-            for (BakedQuad quad : getModel(normalState).getQuads(normalState, side, rand))
+        IBlockState teMirrorState = ((TileEntityInfomationHolder) BaseTERender.currentWorld.getTileEntity(BaseTERender.currentPos)).getMirrorState();
+        IBakedModel teMirrorModel = getModel(teMirrorState);
+        IBlockState s = BaseTERender.currentRender;
+        IBlockState normalState = Blocks.DARK_OAK_DOOR.getDefaultState().withProperty(BlockDoor.FACING, s.getValue(BlockDoor.FACING)).withProperty(BlockDoor.HALF, s.getValue(BlockDoor.HALF)).withProperty(BlockDoor.HINGE, s.getValue(BlockDoor.HINGE)).withProperty(BlockDoor.OPEN, s.getValue(BlockDoor.OPEN)).withProperty(BlockDoor.POWERED, s.getValue(BlockDoor.POWERED));
+        for (BakedQuad quad : getModel(normalState).getQuads(normalState, side, rand))
+        {
+            List<BakedQuad> secList = new ArrayList<>(teMirrorModel.getQuads(teMirrorState, side, rand));
+            if (secList.isEmpty())
             {
-                List<BakedQuad> secList = new ArrayList<>(renderInfo.renderModel.getQuads(renderInfo.blockstate, side, rand));
-                if (secList.isEmpty())
-                    for (EnumFacing facing : fallbackOrder())
-                        if (!renderInfo.renderModel.getQuads(renderInfo.blockstate, facing, rand).isEmpty())
-                            secList = renderInfo.renderModel.getQuads(renderInfo.blockstate, facing, rand);
-                for (BakedQuad mirrorQuad : secList)
+                EnumFacing[] fallbackOrder = new EnumFacing[EnumFacing.VALUES.length + 1];
+                System.arraycopy(EnumFacing.VALUES, 0, fallbackOrder, 0, EnumFacing.VALUES.length);
+                fallbackOrder[EnumFacing.VALUES.length] = null;
+                for (EnumFacing facing : fallbackOrder)
                 {
-                    int[] vList = new int[quad.getVertexData().length];
-                    System.arraycopy(quad.getVertexData(), 0, vList, 0, vList.length);
-                    int[] sList = mirrorQuad.getVertexData();
-                    int[] cList = {4, 5, 11, 12, 18, 19, 25, 26};
-                    if (sList != null)
-                        for (int i : cList)
-                            vList[i] = sList[i];
-                    finalList.add(new BakedQuad(vList, mirrorQuad.getTintIndex(), quad.getFace(),
-                            Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getTexture(state), mirrorQuad.shouldApplyDiffuseLighting(), mirrorQuad.getFormat()));
+                    if (!teMirrorModel.getQuads(teMirrorState, facing, rand).isEmpty()) secList = teMirrorModel.getQuads(teMirrorState, facing, rand);
                 }
             }
-        return finalList;
-    }
-
-    public static class RenderInfo
-    {
-        public final IBlockState blockstate;
-        public final IBakedModel renderModel;
-
-        public RenderInfo(IBlockState blockstate, IBakedModel renderModel)
-        {
-            this.blockstate = blockstate;
-            this.renderModel = renderModel;
+            for (BakedQuad mirrorQuad : secList)
+            {
+                int[] vList = new int[quad.getVertexData().length];
+                System.arraycopy(quad.getVertexData(), 0, vList, 0, vList.length);
+                int[] sList = mirrorQuad.getVertexData();
+                int[] cList = {4, 5, 11, 12, 18, 19, 25, 26};
+                if (sList != null)
+                {
+                    for (int i : cList) vList[i] = sList[i];
+                }
+                finalList.add(new BakedQuad(vList, mirrorQuad.getTintIndex(), quad.getFace(), Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getTexture(state), mirrorQuad.shouldApplyDiffuseLighting(), mirrorQuad.getFormat()));
+            }
         }
+        return finalList;
     }
 }
